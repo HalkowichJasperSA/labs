c14241785d2fe05fd15ba6bd47a12f29899b0f28
Chris Bradfield <chris@kidscancode.org>
2020-09-05 21:34:56 -0700

    add more labs

---
+++ population/population.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt for start size
+    int start = get_int("Start size: ");
+
+    // TODO: Ensure start size at least 9
+
+    // Prompt for end size
+    int end = get_int("End size: ");
+
+    // TODO: Ensure end size greater than start size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
