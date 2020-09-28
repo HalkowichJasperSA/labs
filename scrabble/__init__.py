77330602dac5dd256ea4fe09706141fc1ca32682
Chris Bradfield <chris@kidscancode.org>
2020-09-28 11:49:32 -0700

    updates

--- scrabble/__init__.py
+++ scrabble/__init__.py
@@ -1,48 +1,56 @@
 import check50
-import check50.c
+
 
 @check50.check()
 def exists():
-    """scrabble.c exists"""
-    check50.exists("scrabble.c")
+    """scrabble.py exists"""
+    check50.exists("scrabble.py")
+
 
 @check50.check(exists)
-def compiles():
-    """scrabble.c compiles"""
-    check50.c.compile("scrabble.c", lcs50=True)
-
-@check50.check(compiles)
 def tie_letter_case():
     """handles letter cases correctly"""
-    check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout("Tie!").exit(0)
+    check50.run("python3 scrabble.py").stdin(
+        "LETTERCASE").stdin("lettercase").stdout("Tie!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def tie_punctuation():
     """handles punctuation correctly"""
-    check50.run("./scrabble").stdin("Punctuation!?!?").stdin("punctuation").stdout("Tie!").exit(0)
+    check50.run("python3 scrabble.py").stdin(
+        "Punctuation!?!?").stdin("punctuation").stdout("Tie!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def test1():
     """correctly identifies 'Question?' and 'Question!' as a tie"""
-    check50.run("./scrabble").stdin("Question?").stdin("Question!").stdout("Tie!").exit(0)
+    check50.run("python3 scrabble.py").stdin(
+        "Question?").stdin("Question!").stdout("Tie!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def test2():
     """correctly identifies 'hai!' as winner over 'Oh,'"""
-    check50.run("./scrabble").stdin("Oh,").stdin("hai!").stdout("Player 2 wins!").exit(0)
+    check50.run("python3 scrabble.py").stdin("Oh,").stdin(
+        "hai!").stdout("Player 2 wins!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def test3():
     """correctly identifies 'COMPUTER' as winner over 'science'"""
-    check50.run("./scrabble").stdin("COMPUTER").stdin("science").stdout("Player 1 wins!").exit(0)
+    check50.run("python3 scrabble.py").stdin("COMPUTER").stdin(
+        "science").stdout("Player 1 wins!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def test4():
     """correctly identifies 'Scrabble' as winner over 'wiNNeR'"""
-    check50.run("./scrabble").stdin("Scrabble").stdin("wiNNeR").stdout("Player 1 wins!").exit(0)
+    check50.run("python3 scrabble.py").stdin("Scrabble").stdin(
+        "wiNNeR").stdout("Player 1 wins!").exit(0)
 
-@check50.check(compiles)
+
+@check50.check(exists)
 def complex_case():
     """correctly identifies 'Skating!' as winner over 'figure?'"""
-    check50.run("./scrabble").stdin("figure?").stdin("Skating!").stdout("Player 2 wins!").exit(0)
-
+    check50.run("python3 scrabble.py").stdin("figure?").stdin(
+        "Skating!").stdout("Player 2 wins!").exit(0)
